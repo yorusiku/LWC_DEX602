ac8db023af84dbca58d1d7b6afb438ab20cace4e
FIRST_NAME LAST_NAME <leejaeyoel1@dex450.240103i2m14.com.minipjdev>
2024-02-21 12:30:58 +0900

    Exercise 6-4 Edit Record

--- force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
+++ force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
@@ -64,6 +64,13 @@ export default class TripReportBrowser extends LightningElement {
 	onBtnNewClick() {
 		this.changeTripReportMode('add');
 	}
+
+	handleRowClick(event) {
+		this.selectedRecordId = event.detail.pk;
+	}
+	handleRowDblClick() {
+	this.changeTripReportMode('edit');
+	}
 	
 
 }
